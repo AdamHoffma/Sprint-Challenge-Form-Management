b2be643d944c6e757a6b657683ee421fde733092
Adam Hoffman <wintros@yahoo.com>
2019-08-09 11:59:15 -0600

    Form, test WIP

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import FormikForm from "./Form.js"
+
+function App() {
+  return (
+    <div className="App">
+     <FormikForm/>
+    </div>
+  );
+}
+
+export default App;
